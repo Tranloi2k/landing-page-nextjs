647362357b0bab28d19f1f23e56e3b14623a4232
Loi Tran <tranloi20001007@gmail.com>
2025-03-18 14:57:24 +0700

    remove hardcode

--- app/login/loginForm.tsx
+++ app/login/loginForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import axios from "axios";
 import axiosInstance from "@/config/axios";
 
 const LoginForm = () => {
@@ -13,17 +12,12 @@ const LoginForm = () => {
 
     try {
       await axiosInstance.post(
-        "http://localhost:5000/auth/login",
+        "auth/login",
         { email, password },
         {
           withCredentials: true, // Đảm bảo gửi cookie
         }
       );
-      await fetch("/api/auth", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ token: "abc123" }),
-      });
       // Lưu token vào cookie
       setMessage("Login successful!");
     } catch (error) {
@@ -33,7 +27,7 @@ const LoginForm = () => {
 
   const fetchProtectedData = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/protected", {
+      const response = await axiosInstance.get("protected", {
         withCredentials: true, // Đảm bảo gửi cookie
       });
       setMessage(response.data);
